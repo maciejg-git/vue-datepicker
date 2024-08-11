2e72b8acc1aa14c7f9f2536904c5637064378f8b
maciejg-git <ender@localhost>
2024-08-12 01:39:51 +0200

    update

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -122,8 +122,20 @@
       >
     </tabs-bar>
     <tabs-container class="p-4">
-      <tab name="tab 2"> tab 2 </tab>
-      <tab name="tab 3"> tab 3 </tab>
+      <tab name="tab 2"> Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum. </tab>
+      <tab name="tab 3"> It is a long established fact that a reader will be distracted by the
+      readable content of a page when looking at its layout. The point of using
+      Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+      as opposed to using 'Content here, content here', making it look like
+      readable English </tab>
       <tab name="tab 4"> tab 4 </tab>
       <tab v-for="tab in tabsData" :name="tab.name"> {{ tab.name }} </tab>
     </tabs-container>
