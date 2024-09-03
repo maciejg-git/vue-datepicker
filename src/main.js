675640cc5546812e9ee6d173fe015a66bcec6799
maciejg-git <ender@localhost>
2024-09-03 14:00:49 +0200

    add tabs plugin

--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
 import './assets/main.css'
 import "./tailwind.css"
 import DatepickerPlugin from "./components/datepicker"
-import Tabs from "./components/tabs/Tabs.vue"
-import TabsBar from "./components/tabs/TabsBar.vue"
-import TabsBarItem from "./components/tabs/TabsBarItem.vue"
-import TabsContainer from "./components/tabs/TabsContainer.vue"
-import Tab from "./components/tabs/Tab.vue"
+import TabsPlugin from "./components/tabs"
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 let app = createApp(App)
 app.use(DatepickerPlugin)
-app.component("Tabs", Tabs)
-app.component("TabsBar", TabsBar)
-app.component("TabsBarItem", TabsBarItem)
-app.component("TabsContainer", TabsContainer)
-app.component("Tab", Tab)
+app.use(TabsPlugin)
 app.mount('#app')
